fd40a5ec407ef4f3fa03542f06f188cdc9a23ca4
yungo1846 <yungo1468@naver.com>
2020-08-23 14:15:46 +0900

    exception

--- Weather.js
+++ Weather.js
@@ -4,61 +4,81 @@ import propTypes from "prop-types";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
 
-const weatherOptions = {
+var weatherOptions = {
   Thunderstorm: {
+    name: "Thunderstorm",
     iconName: "weather-lightning",
     gradient: ["#373B44", "#4286f4"],
     subtitle: "Don't go outside!",
   },
   Drizzle: {
+    name: "Drizzle",
     iconName: "weather-rainy",
     gradient: ["#3a7bd5", "#3a6073"],
     subtitle: "Bring your umbrella!",
   },
   Rain: {
+    name: "Rain",
     iconName: "weather-rainy",
     gradient: ["#4B79A1", "#283E51"],
     subtitle: "Bring your umbrella!",
   },
   Snow: {
+    name: "Snow",
     iconName: "weather-snowy",
     gradient: ["#1c92d2", "#f2fcfe"],
     subtitle: "Beware of slipping!",
   },
   Clear: {
+    name: "Clear",
     iconName: "white-balance-sunny",
     gradient: ["#FF4E50", "#F9D423"],
     subtitle: "It's sunny day.",
   },
   Clouds: {
+    name: "Clouds",
     iconName: "cloud",
     gradient: ["#bdc3c7", "#2c3e50"],
     subtitle: "It's cloudy.",
   },
   Dust: {
+    name: "Dust",
     iconName: "weather-hazy",
     gradient: ["#636363", "#a2ab58"],
     subtitle: "Put on a mask.",
   },
   Fog: {
+    name: "Fog",
     iconName: "weather-fog",
     gradient: ["#4DA0B0", "#D39D38"],
     subtitle: "Be careful driving.",
   },
   Haze: {
+    name: "Haze",
     iconName: "weather-hazy",
     gradient: ["#70e1f5", "#ffd194"],
     subtitle: "Be careful driving.",
   },
   Mist: {
+    name: "Mist",
     iconName: "water",
     gradient: ["#50C9C3", "#96DEDA"],
     subtitle: "It's humid.",
+  },
+  Etc: {
+    name: "Etc",
+    iconName: "exclamation",
+    gradient: ["#373B44", "#4286f4"],
+    subtitle: "Don't go outside!",
   },
 };
 
 // es6에서 두가지의 object를 함께 쓰는 방법으로 ...이 있다. line 76 참고
 export default function Weather({ temp, condition }) {
+  if (weatherOptions[condition] === undefined) {
+    weatherOptions["Etc"].name = condition;
+    condition = "Etc";
+  }
   return (
     <LinearGradient
       colors={weatherOptions[condition].gradient}
@@ -70,7 +90,7 @@ export default function Weather({ temp, condition }) {
           size={96}
           color="white"
         />
-        <Text style={styles.weather}>{condition}</Text>
+        <Text style={styles.weather}>{weatherOptions[condition].name}</Text>
         <Text style={styles.temp}>{temp}°C</Text>
       </View>
       <View style={styles.textContainer}>
